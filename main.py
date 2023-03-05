faae586a6ff0ea38eca8488238c8eac56fa75ed9
Renê Lemos <renelemoss@hotmail.com>
2023-03-05 19:53:44 -0300

    Break changes on timer mechanism
    Break changes on timer mechanism

--- main.py
+++ main.py
@@ -10,14 +10,27 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
+    global reps
+    reps += 1
     
-    count_down(5*60)
     
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+    elif reps % 2 ==0:
+        count_down(short_break_sec)
+    else:
+        count_down(work_sec)
+        
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     
